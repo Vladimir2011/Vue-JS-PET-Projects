c122a3ad72b0985d7fd34bb6bdd2dd709f8430d9
Vladimir Garbuzov <otmopo3ehb@gmail.com>
2021-04-05 13:07:14 +0300

    first commit

---
+++ Mini-bank-app/src/layout/MainLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <the-navbar />
+  <the-sidebar />
+  <div class="container with-nav">
+    <app-message />
+    <router-view />
+  </div>
+</template>
+
+<script>
+
+import TheNavbar from "../components/TheNavbar";
+import AppMessage from "../components/ui/AppMessage";
+import TheSidebar from "../components/TheSidebar"
+export default {
+  components: {AppMessage, TheNavbar, TheSidebar}
+}
+
+</script>
+
+<style scoped>
+
+</style>
